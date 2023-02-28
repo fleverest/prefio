872259dfb2f66a580a4ec695945554acb299e9ec
Floyd Everest <me@floydeverest.com>
2023-02-28 13:30:02 +1100

    Add `from_preflib` and separate documentation of `read.preflib` and `write.preflib`. (#12)
    * Separated the documentation of `read.preflib` and `write.preflib`.
    
    added a `from_preflib` argument to `read.preflib` which, when TRUE, will try to source data from PrefLib over http
    
    * remove rdname `preflib` from the `read.preflib` roxygen

--- R/preflib.R
+++ R/preflib.R
@@ -30,10 +30,8 @@
 #'
 #' @param file A preferential data file, conventionally with extension `.soc`,
 #' `.soi`, `.toc` or `.toi` according to data type.
-#' @param x An object of class `preflib`.
-#' @param as.aggregated_preferences When `TRUE`, returns an object of class
-#' [`aggregated_preferences`][aggregate.preferences]. When `FALSE`, returns
-#' a `data.frame` containing the raw PrefLib data.
+#' @param from_preflib A logical which, when `TRUE` will attempt to source
+#' the file from PrefLib by adding the database `HTTP` prefix.
 #' @param ... Additional arguments passed to [as.aggregated_preferences()]:
 #' `frequencies`, `format` or `item_names` will be ignored as they are
 #' set automatically.
@@ -69,138 +67,139 @@
 #' cities <- read.preflib(file.path(preflib, "cities/00034-00000001.soi"))
 #' }
 #' @importFrom utils read.csv
-#' @name preflib
-NULL
-
-#' @rdname preflib
 #' @export
-read.preflib <- function(file) {
-    # Read the file into memory
-    lines <- readLines(file)
+read.preflib <- function(file, from_preflib = FALSE) {
+  if (from_preflib) {
+    file <- file.path(preflib_url, file)
+  }
+  # Read the file into memory
+  lines <- readLines(file)
 
-    # Filter the header lines and convert them to key:value attributes
-    header_lines <- sub("# ", "", grep("^#", lines, value = TRUE))
-    metadata <- regmatches(
-      header_lines,
-      regexpr(": ", header_lines),
-      invert = TRUE
-    )
-    preflib_attributes <- lapply(metadata, function(x) x[2])
-    names(preflib_attributes) <- sapply(metadata, function(x) x[1])
+  # Filter the header lines and convert them to key:value attributes
+  header_lines <- sub("# ", "", grep("^#", lines, value = TRUE))
+  metadata <- regmatches(
+    header_lines,
+    regexpr(": ", header_lines),
+    invert = TRUE
+  )
+  preflib_attributes <- lapply(metadata, function(x) x[2])
+  names(preflib_attributes) <- sapply(metadata, function(x) x[1])
 
-    # Assert that the minimum required attributes are present
-    required_attributes <- c("NUMBER UNIQUE ORDERS",
-                             "NUMBER VOTERS",
-                             "NUMBER ALTERNATIVES")
+  # Assert that the minimum required attributes are present
+  required_attributes <- c("NUMBER UNIQUE ORDERS",
+                           "NUMBER VOTERS",
+                           "NUMBER ALTERNATIVES")
 
-    if (suppressWarnings(
-          anyNA(as.integer(unlist(preflib_attributes[required_attributes]))))) {
-      stop("PrefLib datafile is corrupt: 'NUMBER UNIQUE ORDERS', ",
-           "'NUMBER VOTERS' and 'NUMBER ALTERNATIVES' must be able to be ",
-           "coerced into integers.")
-    }
-    if (!"NUMBER ALTERNATIVES" %in% names(preflib_attributes)) {
-      stop(paste0("PrefLib datafile is corrupt: ",
-                  "missing 'NUMBER ALTERNATIVES' metadata."))
-    }
-    n_alternatives <- as.integer(preflib_attributes[["NUMBER ALTERNATIVES"]])
-    required_attributes <- c(required_attributes,
-                             paste("ALTERNATIVE NAME",
-                                   seq_len(n_alternatives)))
-    if (any(!required_attributes %in% names(preflib_attributes))) {
-      stop(paste0("PrefLib datafile is corrupt: ",
-                  "missing required metadata (",
-                  paste(required_attributes[
-                      which(!required_attributes %in% names(preflib_attributes))
-                    ]
-                  ),
-                  ")."))
-    }
+  if (suppressWarnings(
+        anyNA(as.integer(unlist(preflib_attributes[required_attributes]))))) {
+    stop("PrefLib datafile is corrupt: 'NUMBER UNIQUE ORDERS', ",
+         "'NUMBER VOTERS' and 'NUMBER ALTERNATIVES' must be able to be ",
+         "coerced into integers.")
+  }
+  if (!"NUMBER ALTERNATIVES" %in% names(preflib_attributes)) {
+    stop(paste0("PrefLib datafile is corrupt: ",
+                "missing 'NUMBER ALTERNATIVES' metadata."))
+  }
+  n_alternatives <- as.integer(preflib_attributes[["NUMBER ALTERNATIVES"]])
+  required_attributes <- c(required_attributes,
+                           paste("ALTERNATIVE NAME",
+                                 seq_len(n_alternatives)))
+  if (any(!required_attributes %in% names(preflib_attributes))) {
+    stop(paste0("PrefLib datafile is corrupt: ",
+                "missing required metadata (",
+                paste(required_attributes[
+                    which(!required_attributes %in% names(preflib_attributes))
+                  ]
+                ),
+                ")."))
+  }
 
-    # Concatenate all the 'ALTERNATIVE NAME X' into a list of names
-    alternative_names <- unname(unlist(
-      preflib_attributes[
-        paste(
-          "ALTERNATIVE NAME",
-          seq_len(n_alternatives)
-        )
-      ]
-    ))
+  # Concatenate all the 'ALTERNATIVE NAME X' into a list of names
+  alternative_names <- unname(unlist(
+    preflib_attributes[
+      paste(
+        "ALTERNATIVE NAME",
+        seq_len(n_alternatives)
+      )
+    ]
+  ))
 
-    # Filter the data lines and 'encourage' them into a csv format
-    data_lines <- grep("^[^#]", lines, value = TRUE)
-    data_lines <- chartr("{}", "''", data_lines)
-    csv_string <- paste(
-      sub(
-        "^([0-9]+): ",
-        "\\1,",
-        data_lines
-      ),
-      collapse = "\n"
-    )
-    preferences <- read.csv(text = csv_string,
-                            header = FALSE,
-                            stringsAsFactors = FALSE,
-                            strip.white = TRUE,
-                            quote = "'",
-                            col.names = paste0("V", 1:(n_alternatives + 1)),
-                            colClasses = rep("character", n_alternatives))
-    # Replace character columns with lists
-    preferences[, seq(2, ncol(preferences))] <- as.data.frame(
-      do.call(
-        rbind,
-        apply(
-          preferences[, seq(2, ncol(preferences))],
-          1L,
-          strsplit,
-          split = ","
-        )
+  # Filter the data lines and 'encourage' them into a csv format
+  data_lines <- grep("^[^#]", lines, value = TRUE)
+  data_lines <- chartr("{}", "''", data_lines)
+  csv_string <- paste(
+    sub(
+      "^([0-9]+): ",
+      "\\1,",
+      data_lines
+    ),
+    collapse = "\n"
+  )
+  preferences <- read.csv(text = csv_string,
+                          header = FALSE,
+                          stringsAsFactors = FALSE,
+                          strip.white = TRUE,
+                          quote = "'",
+                          col.names = paste0("V", 1:(n_alternatives + 1)),
+                          colClasses = rep("character", n_alternatives))
+  # Replace character columns with lists
+  preferences[, seq(2, ncol(preferences))] <- as.data.frame(
+    do.call(
+      rbind,
+      apply(
+        preferences[, seq(2, ncol(preferences))],
+        1L,
+        strsplit,
+        split = ","
       )
     )
-    # Convert all data in the output to integer-valued vectors
-    preferences <- rapply(
-      preferences,
-      as.integer,
-      how = "replace"
-    )
-    names(preferences) <- c(
-      "Frequency",
-      paste("Rank", seq_len(ncol(preferences)  - 1))
-    )
-    frequencies <- preferences[, 1]
-    preferences <- preferences[, -1]
-    preferences <- as.preferences(
-      preferences,
-      "ordering",
-      item_names = alternative_names
-    )
-    aggregated_prefs <- aggregate(preferences, frequencies = frequencies)
-    attr(aggregated_prefs, "preflib") <- preflib_attributes
+  )
+  # Convert all data in the output to integer-valued vectors
+  preferences <- rapply(
+    preferences,
+    as.integer,
+    how = "replace"
+  )
+  names(preferences) <- c(
+    "Frequency",
+    paste("Rank", seq_len(ncol(preferences)  - 1))
+  )
+  frequencies <- preferences[, 1]
+  preferences <- preferences[, -1]
+  preferences <- as.preferences(
+    preferences,
+    "ordering",
+    item_names = alternative_names
+  )
+  aggregated_prefs <- aggregate(preferences, frequencies = frequencies)
+  attr(aggregated_prefs, "preflib") <- preflib_attributes
 
-    # Ensure we have the expected number of unique and total orderings.
-    n_unique_orders <- as.integer(preflib_attributes[["NUMBER UNIQUE ORDERS"]])
-    n_voters <- as.integer(preflib_attributes[["NUMBER VOTERS"]])
-    if (length(aggregated_prefs$preferences) != n_unique_orders) {
-      warning(paste0("Expected ",
-                     n_unique_orders,
-                     " unique orderings but only ",
-                     length(aggregated_prefs$preferences),
-                     " were recovered when reading the PrefLib datafile.",
-                     " The file may be corrupt."
-                      ))
-    }
-    if (sum(aggregated_prefs$frequencies) != n_voters) {
-      warning(paste0("Expected ",
-                     n_voters,
-                     " total orderings but only ",
-                     sum(aggregated_prefs$frequencies),
-                     " were recovered when reading the PrefLib datafile.",
-                     " The file may be corrupt."
-                      ))
-    }
+  # Ensure we have the expected number of unique and total orderings.
+  n_unique_orders <- as.integer(preflib_attributes[["NUMBER UNIQUE ORDERS"]])
+  n_voters <- as.integer(preflib_attributes[["NUMBER VOTERS"]])
+  if (length(aggregated_prefs$preferences) != n_unique_orders) {
+    warning(paste0("Expected ",
+                   n_unique_orders,
+                   " unique orderings but only ",
+                   length(aggregated_prefs$preferences),
+                   " were recovered when reading the PrefLib datafile.",
+                   " The file may be corrupt."
+                    ))
+  }
+  if (sum(aggregated_prefs$frequencies) != n_voters) {
+    warning(paste0("Expected ",
+                   n_voters,
+                   " total orderings but only ",
+                   sum(aggregated_prefs$frequencies),
+                   " were recovered when reading the PrefLib datafile.",
+                   " The file may be corrupt."
+                    ))
+  }
 
-    return(aggregated_prefs)
+  return(aggregated_prefs)
 }
+
+preflib_url <- "https://www.preflib.org/static/data"
 
 #' Write Ordinal Preference Data to PrefLib Formats
 #'
