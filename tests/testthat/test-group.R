0766bc5c2d78045d5d7fe572143d0dd1eec9d860
Floyd Everest <me@floydeverest.com>
2023-03-01 17:02:57 +1100

    Added functionality for grouping preferences by a factor (#15)
    * Added functionality for grouping preferences by a factor, producing neat formats with names for each group.
    
    * added an example for grouping by factors

--- tests/testthat/test-group.R
+++ tests/testthat/test-group.R
@@ -20,9 +20,22 @@ test_that("Can construct `grouped_preferences` from `preferences`", {
   expect_true(all(attr(gprefs[1], "preferences") == prefs[1:2]))
 })
 
+test_that("`preferences` can be grouped by a factor and accessed by name", {
+  prefs <- preferences(rankings, format = "ranking")
+  gprefs <- group(prefs,
+                  as.factor(rep(paste("Group", LETTERS[24:26]), each = 2)))
+  expect_true(all(attr(gprefs["Group X"], "preferences") == prefs[1:2]))
+  expect_true(all(attr(gprefs["Group Y"], "preferences") == prefs[3:4]))
+  expect_true(all(attr(gprefs["Group Z"], "preferences") == prefs[5:6]))
+})
+
 test_that("`print.grouped_preferences` formats correctly", {
   prefs <- preferences(rankings, format = "ranking")
-  gprefs <- group(prefs, rep(1:3, each = 2))
+  gprefs <- group(prefs,
+                  as.factor(rep(paste("Group", LETTERS[24:26]), each = 2)))
+  expect_output(print(gprefs), 'Group X')
+  expect_output(print(gprefs), 'Group Y')
+  expect_output(print(gprefs), 'Group Z')
   expect_output(print(gprefs), '"A > B > C, A > C > B"')
   expect_output(print(gprefs), '"B > A > C, C > A > B"')
   expect_output(print(gprefs), '"B > C > A, C > B > A"')
@@ -32,7 +45,8 @@ test_that("`print.grouped_preferences` formats correctly", {
 
 test_that("`as.data.frame` produces one row of grouped_preferences per index", {
   prefs <- preferences(rankings, format = "ranking")
-  gprefs <- group(prefs, rep(1:3, each = 2))
+  gprefs <- group(prefs,
+                  as.factor(rep(paste("Group", LETTERS[24:26]), each = 2)))
   df <- as.data.frame(gprefs)
   expect_true(all(dim(df) == c(3, 1)))
   expect_true(class(df[1, ]) == "grouped_preferences")
